8d08872f7acefcfef0fba12142713b79d4871f11
James Platt <james.platt@neptune.com>
2018-01-09 13:47:15 +0000

    Concurrency test now produces deadlocks

---
+++ Multithreaded-And-Asynchronous-Programming/ConcurrencyTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreaded_And_Asynchronous_Programming
+{
+    class ConcurrencyTest
+    {
+        private class DataStore { public int Value { get; set; } }
+
+        private DataStore store = new DataStore();
+        private DataStore store2 = new DataStore();
+        private DataStore sum = new DataStore();
+
+        public void Test()
+        {
+            var thread1 = new Thread(IncrementTheValues);
+            var thread2 = new Thread(SumStores);
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join(); // Wait for the thread to finish executing
+            thread2.Join();
+
+            Console.WriteLine($"Final value: {sum.Value}");
+        }
+
+        private void IncrementTheValues()
+        {
+            lock (store2)
+            {
+                lock (store)
+                {
+                    store2.Value++;
+                    store.Value++;
+                }
+            }
+        }
+        private void SumStores()
+        {
+            lock (store)
+            {
+                lock (store2)
+                {
+                    sum.Value += store.Value;
+                }
+            }
+        }
+    }
+}
---
+++ Multithreaded-And-Asynchronous-Programming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Multithreaded_And_Asynchronous_Programming
+{
+    class Program
+    {
+        static void Main()
+        {
+            var concurrencyTest = new ConcurrencyTest();
+            while (true)
+            {
+                concurrencyTest.Test();
+            }
+        }
+    }
+}
