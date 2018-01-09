664863e8050db786f3313bee5eba2e435c04c401
James Platt <james.platt@neptune.com>
2018-01-09 13:49:21 +0000

    Count non existent words added, uses threads instead of tasks so that the pages download simultaneously.

---
+++ Multithreaded-And-Asynchronous-Programming/NonExistentWordCounter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Multithreaded_And_Asynchronous_Programming
+{
+    class NonExistentWordCounter
+    {
+        private ConcurrentQueue<string> articleWords = new ConcurrentQueue<string>();
+        private HashSet<string> validWords = new HashSet<string>();
+        private string article;
+        private string words;
+
+
+        private void ReadArticle()
+        {
+            article = new WebClient().DownloadStringTaskAsync(
+              @"https://msdn.microsoft.com/en-gb/library/mt674882.aspx").Result;
+        }
+
+        private void ReadWords()
+        {
+            words = new WebClient().DownloadStringTaskAsync(
+              @"https://raw.githubusercontent.com/dwyl/english-words/master/words.txt").Result;
+        }
+
+        public async Task<int> CountNonExistentWordsAsync()
+        {
+            var thread1 = new Thread(ReadArticle);
+            var thread2 = new Thread(ReadWords);
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            HashSet<string> wordList = new HashSet<string>(words.Split('\n'));
+
+            int nonExistentWords = article.Split('\n', ' ').Count(n => !wordList.Contains(n));
+
+            return nonExistentWords;
+        }
+    }
+}
--- Multithreaded-And-Asynchronous-Programming/Program.cs
+++ Multithreaded-And-Asynchronous-Programming/Program.cs
@@ -1,16 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace Multithreaded_And_Asynchronous_Programming
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
+        {
+            string input;
+            int val =0;
+            while (val != 3)
+            {
+                PrintMenu();
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out val)) { continue; }
+
+                switch (val)
+                {
+                    case 1:
+                        DoConcurrencyTest();
+                        break;
+                    case 2:
+                        DoNonExistentWordCounter();
+                        break;
+                }
+            }
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" 1. Concurrency Test");
+            Console.WriteLine(" 2. Non existent word count");
+            Console.WriteLine(" 3. Exit");
+        }
+
+        private static void DoConcurrencyTest()
         {
             var concurrencyTest = new ConcurrencyTest();
             while (true)
             {
                 concurrencyTest.Test();
             }
+        }
+
+        private static async void DoNonExistentWordCounter()
+        {
+            var nonExistentWordCounter = new NonExistentWordCounter();
+            Task<int> counter = nonExistentWordCounter.CountNonExistentWordsAsync();
+            int nonExistentWords = await counter;
+
+            Console.WriteLine(nonExistentWords);
         }
     }
 }
